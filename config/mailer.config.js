cbc3bbeb31b055a80fdc39b41e919e836804c4e2
Andrea Astorga <andrea.astorga95@gmail.com>
2020-09-13 11:28:52 +0200

    All CSS

--- config/mailer.config.js
+++ config/mailer.config.js
@@ -21,11 +21,33 @@ module.exports.sendValidationEmail = ({ name, email, id, activationToken }) => {
 		from: `AdoptMe Team <${user}>`,
 		subject: 'Please, activate your account!',
 		html: `
-			<h1>Hi ${name}</h1>
-			<p>Click on the button below to activate your AdoptMe account!</p>
-			<a href="${host}/users/${id}/activate/${activationToken}" style="padding: 10px 20px; color: white; background-color: green; border-radius: 5px;">Click here</a>
+			<table cellpadding="60" cellspacing="0" border="0" width="100%">
+				<tr bgcolor="#1C9569">
+					<td align="center">
+					<p style="padding-bottom:60px">🐶</p>
+					<table cellpadding="60" cellspacing="0" border="0" width="50%" align="center">
+						<tr bgcolor="#fff">
+						<td align="center">
+						<img src="/images/dark-logo-lyr.png" width="200">
+						<h1>AdoptMe confirmation Email</h1>
+						<h4>Hello ${name}!</h4>
+						<p>Thanks to join our community! Please confirm your account by clicking on the following link:</p>
+						<a href="${host}/users/${id}/activate/${activationToken}" style="padding: 10px 20px; color: white; background-color: green; border-radius: 5px;">Click here</a>
+						<h4>Goog luck finding your future pet and thank you for trusting in us!</h4>
+						</td>
+						</tr>
+					</table>
+					<img src="/images/dark-logo.png" width="200">
+					<p style="padding-top:60px">You are doing awesome!</p>
+					</td>
+				</tr>
+			</table>
 		`
 	})
 		.then(console.log)
 		.catch(console.error)
 }
+
+// <h1>Hi ${name}</h1>
+{/* <p>Click on the button below to activate your AdoptMe account!</p> */}
+{/* <a href="${host}/users/${id}/activate/${activationToken}" style="padding: 10px 20px; color: white; background-color: green; border-radius: 5px;">Click here</a> */}
